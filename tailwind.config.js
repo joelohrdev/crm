45642de7e2556fc8fbe62032a7453b5bf5efaf73
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2022-08-07 09:53:22 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'tkd-blue': {
+                    '50': '#f5fbff',
+                    '100': '#eaf8ff',
+                    '200': '#cbedff',
+                    '300': '#abe3ff',
+                    '400': '#6dcdff',
+                    '500': '#2eb8ff',
+                    '600': '#29a6e6',
+                    '700': '#238abf',
+                    '800': '#1c6e99',
+                    '900': '#175a7d'
+                }
+            }
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
