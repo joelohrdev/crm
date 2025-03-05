6c47d55e1971910b11766228047a26440f3650af
Joe <joe@MacBookPro.attlocal.net>
2025-03-05 09:55:23 -0600

    Company show update

--- app/Livewire/Companies/Show.php
+++ app/Livewire/Companies/Show.php
@@ -2,11 +2,12 @@
 
 namespace App\Livewire\Companies;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Show extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.companies.show');
     }
