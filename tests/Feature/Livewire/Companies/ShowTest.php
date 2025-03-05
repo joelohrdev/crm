f280da6039597bdce2c8b6d0b80b8d6dbb43dd04
Joe <joe@MacBookPro.attlocal.net>
2025-03-05 13:01:19 -0600

    WIP Company show test

--- tests/Feature/Livewire/Companies/ShowTest.php
+++ tests/Feature/Livewire/Companies/ShowTest.php
@@ -1,1 +1,17 @@
 <?php
+
+use App\Models\Company;
+use App\Models\User;
+
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+test('a company can be shown', function () {
+    $this->actingAs(User::factory()->create());
+
+    $company = Company::factory()->create();
+
+    $response = $this->get(route('companies.show', $company));
+
+    $response->assertStatus(200);
+    $response->assertSee($company->name);
+});
