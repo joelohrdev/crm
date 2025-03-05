947bbf924ec5cd1b6ed7aa802c797e25856ac25e
Joe <joe@MacBookPro.attlocal.net>
2025-03-05 14:46:54 -0600

    Theme color change and darkmode fix for avatar component

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,5 +1,5 @@
 <span
-    class="inline-flex size-8 items-center justify-center rounded-full border border-gray-200 bg-white text-xs font-semibold text-gray-800 shadow-2xs"
+    class="inline-flex size-8 items-center justify-center rounded-full border border-gray-200 bg-white text-xs font-semibold text-gray-800 shadow-2xs dark:border-neutral-700 dark:bg-neutral-900 dark:text-white"
 >
     {{ $slot }}
 </span>
