1a9e24ed768a17bf6d376776430eda9fa2461531
Joe <joe@MacBookPro.attlocal.net>
2025-03-05 13:05:37 -0600

    Company show test for updating

--- tests/Feature/Livewire/Companies/ShowTest.php
+++ tests/Feature/Livewire/Companies/ShowTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Companies\Show;
 use App\Models\Company;
 use App\Models\User;
 
@@ -14,4 +15,28 @@ test('a company can be shown', function () {
 
     $response->assertStatus(200);
     $response->assertSee($company->name);
+});
+
+test('a company cannot be shown when the user is not authenticated', function () {
+    $company = Company::factory()->create();
+
+    $response = $this->get(route('companies.show', $company));
+
+    $response->assertRedirect(route('login'));
+});
+
+test('company information can be updated', function () {
+    $this->actingAs(User::factory()->create());
+
+    $company = Company::factory()->create();
+
+    $response = Livewire::test(Show::class, ['company' => $company])
+        ->set('form.name', 'New Company Name')
+        ->call('updateCompany');
+
+    $response->assertHasNoErrors();
+
+    $company->refresh();
+
+    expect($company->name)->toBe('New Company Name');
 });
