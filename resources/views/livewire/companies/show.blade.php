5ad9dbeaf8390e67289ce6994803f0bcf798079b
Joe <joe@MacBookPro.attlocal.net>
2025-03-05 16:34:07 -0600

    WIP new layout for company view

--- resources/views/livewire/companies/show.blade.php
+++ resources/views/livewire/companies/show.blade.php
@@ -1,6 +1,17 @@
 <div>
     <div class="flex items-center justify-between">
-        <flux:heading size="xl">{{ $company->name }}</flux:heading>
+        <div>
+            <flux:heading size="xl">
+                <div class="flex items-center gap-x-3">
+                    {{ $company->name }}
+                    <flux:button variant="ghost" size="sm" icon="star"></flux:button>
+                </div>
+            </flux:heading>
+            <flux:subheading size="sm">
+                <flux:badge variant="pill" size="sm" class="mr-3">Active</flux:badge>
+                {{ $company->website }}
+            </flux:subheading>
+        </div>
         <flux:button
             wire:navigate
             href="{{ route('companies.index') }}"
@@ -31,88 +42,95 @@
         </flux:button>
     </div>
     <flux:separator variant="subtle" class="my-4" />
-    <div class="mt-6 flex flex-col gap-4 lg:flex-row lg:gap-6">
-        <div class="w-80">
-            <flux:navlist>
-                <flux:navlist.group>
-                    <flux:navlist.item href="{{ route('companies.show', $company) }}">General</flux:navlist.item>
-                    <flux:navlist.item href="#">Contacts</flux:navlist.item>
-                    <flux:navlist.item href="#">Deals</flux:navlist.item>
-                </flux:navlist.group>
-            </flux:navlist>
-        </div>
-        <div class="flex-1 space-y-6">
-            <form wire:submit.prevent="updateCompany" class="space-y-6">
-                <flux:field>
-                    <flux:label badge="Required">Company Name</flux:label>
 
-                    <flux:input wire:model="name" />
-
-                    <flux:error name="name" />
-                </flux:field>
-
-                <flux:field>
-                    <flux:label>Website</flux:label>
-
-                    <flux:input wire:model="website" placeholder="https://example.com" />
-
-                    <flux:error name="website" />
-                </flux:field>
-
-                <flux:field>
-                    <flux:label>Address</flux:label>
-
-                    <flux:input wire:model="address" />
-
-                    <flux:error name="address" />
-                </flux:field>
-
-                <div class="grid grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 gap-6 lg:grid-cols-3">
+        <div class="lg:col-span-2">
+            <flux:tabs class="mb-6 w-full" variant="segmented">
+                <flux:tab>Overview</flux:tab>
+                <flux:tab>Contacts</flux:tab>
+                <flux:tab>Deals</flux:tab>
+                <flux:tab>Activity</flux:tab>
+                <flux:tab>Tasks</flux:tab>
+            </flux:tabs>
+            <div class="rounded-lg border p-6">
+                <flux:heading size="lg">Overview</flux:heading>
+                <form wire:submit.prevent="updateCompany" class="space-y-6">
                     <flux:field>
-                        <flux:label>City</flux:label>
+                        <flux:label badge="Required">Company Name</flux:label>
 
-                        <flux:input wire:model="city" />
+                        <flux:input wire:model="name" />
 
-                        <flux:error name="city" />
+                        <flux:error name="name" />
                     </flux:field>
 
                     <flux:field>
-                        <flux:label>State</flux:label>
-                        <flux:select wire:model="state" variant="listbox" searchable>
-                            @foreach (\App\Enums\State::cases() as $state)
-                                <flux:select.option :value="$state">{{ $state->label() }}</flux:select.option>
+                        <flux:label>Website</flux:label>
+
+                        <flux:input wire:model="website" placeholder="https://example.com" />
+
+                        <flux:error name="website" />
+                    </flux:field>
+
+                    <flux:field>
+                        <flux:label>Address</flux:label>
+
+                        <flux:input wire:model="address" />
+
+                        <flux:error name="address" />
+                    </flux:field>
+
+                    <div class="grid grid-cols-3 gap-6">
+                        <flux:field>
+                            <flux:label>City</flux:label>
+
+                            <flux:input wire:model="city" />
+
+                            <flux:error name="city" />
+                        </flux:field>
+
+                        <flux:field>
+                            <flux:label>State</flux:label>
+                            <flux:select wire:model="state" variant="listbox" searchable>
+                                @foreach (\App\Enums\State::cases() as $state)
+                                    <flux:select.option :value="$state">{{ $state->label() }}</flux:select.option>
+                                @endforeach
+                            </flux:select>
+                        </flux:field>
+
+                        <flux:field>
+                            <flux:label>Zip Code</flux:label>
+
+                            <flux:input wire:model="zip" />
+
+                            <flux:error name="zip" />
+                        </flux:field>
+                    </div>
+
+                    <flux:field>
+                        <flux:label>Phone Number</flux:label>
+
+                        <flux:input type="tel" mask="999-999-9999" wire:model="phone" placeholder="999-999-9999" />
+
+                        <flux:error name="phone" />
+                    </flux:field>
+
+                    <flux:field>
+                        <flux:label>Assign Employees</flux:label>
+                        <flux:select variant="listbox" multiple wire:model="selectedEmployees">
+                            @foreach ($users as $user)
+                                <flux:select.option :value="$user->id">{{ $user->name }}</flux:select.option>
                             @endforeach
                         </flux:select>
                     </flux:field>
 
-                    <flux:field>
-                        <flux:label>Zip Code</flux:label>
-
-                        <flux:input wire:model="zip" />
-
-                        <flux:error name="zip" />
-                    </flux:field>
-                </div>
-
-                <flux:field>
-                    <flux:label>Phone Number</flux:label>
-
-                    <flux:input type="tel" mask="999-999-9999" wire:model="phone" placeholder="999-999-9999" />
-
-                    <flux:error name="phone" />
-                </flux:field>
-
-                <flux:field>
-                    <flux:label>Assign Employees</flux:label>
-                    <flux:select variant="listbox" multiple wire:model="selectedEmployees">
-                        @foreach ($users as $user)
-                            <flux:select.option :value="$user->id">{{ $user->name }}</flux:select.option>
-                        @endforeach
-                    </flux:select>
-                </flux:field>
-
-                <flux:button type="submit" variant="primary">Update</flux:button>
-            </form>
+                    <flux:button type="submit" variant="primary">Update</flux:button>
+                </form>
+            </div>
+        </div>
+        <div class="space-y-6">
+            <div class="rounded-lg border p-6">right</div>
+            <div class="rounded-lg border p-6">right</div>
+            <div class="rounded-lg border p-6">right</div>
         </div>
     </div>
 </div>
